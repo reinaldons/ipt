08a0b49dfe4f5f5a6ecf25cff0ba3dde2ce83003
Camila Sanches <oi326164@oiinternet.corp>
2014-02-19 12:55:10 -0300

    scripts de build

--- soa/ws/bestdog/ws.sh
+++ soa/ws/bestdog/ws.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if [ "$1" == "h2" ]; then
-   H2_DIR=target/bestdog.ws-1.0-SNAPSHOT/WEB-INF/lib/
+   H2_DIR=target/bestdog-1.0-SNAPSHOT/WEB-INF/lib/
    java -cp $H2_DIR/h2*.jar org.h2.tools.RunScript -url jdbc:h2:~/test -script src/test/resources/db.sql
    java -cp $H2_DIR/h2*.jar org.h2.tools.Server
 else
