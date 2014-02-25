6c0e9c055534f820ba55b732da12cd1c4e1e8ad8
vaio@vaio <vaio@vaio>
2014-02-25 00:11:28 -0300

    soapclient JAVA_HOME naming restriction

--- soa/scripts/download_soapclient.sh
+++ soa/scripts/download_soapclient.sh
@@ -1,13 +1,16 @@
 #/bin/bash
-if [ -d /tmp/soapclient ]; then
+if [ -d /tmp/_soapclient ]; then
 echo "soapclient already installed"
 else
+JAVA_HOME=/usr/lib/jvm/java-7-oracle
+cd $JAVA_HOME
+sudo ln -s jre jdk
 cd /tmp
-mkdir soapclient
+mkdir _soapclient
 cd soapclient
 wget http://demo.360works.com/wsm/SOAPClient.zip
 unzip SOAPClient.zip
 rm SOAPClient.zip
 fi
 cd /tmp/soapclient/build
-java -Djava.home=/usr/lib/jvm/java-7-oracle/jre/ -cp *.jar -jar SOAPClient.jar &
+java -Djava.home=$JAVA_HOME/jdk/ -cp *.jar -jar SOAPClient.jar &
